78695b1a7cf928202e3139ad549a9757af1ac5ff
Lei Xu <xlxlx9@gmail.com>
2016-10-10 20:18:49 -0400

    [+] bridging to the openems_mod.jar.

---
+++ Assets/Scripts/TestJar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestJar : MonoBehaviour {
+
+	private AndroidJavaObject mod_fatctory = null;
+	private AndroidJavaObject activityContext = null;
+	// Use this for initialization
+	void Start () {
+		if (null != mod_fatctory)
+			return;
+		using(AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+			activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+		}
+		using(AndroidJavaClass pluginClass = new AndroidJavaClass("com.openems.stim.EMSFactory")) {
+			if (pluginClass != null) {
+				mod_fatctory = pluginClass.CallStatic<AndroidJavaObject>("inst");
+				mod_fatctory.Call("setContext", activityContext);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("mod_factory = " + (null == mod_fatctory ? "NULL" : "Not NULL"));
+	}
+}
